d4f899d816138d0bf78d941e1cfac7b1be0a40f2
Caleb Froese <caleb.froese@gmail.com>
2017-03-03 20:59:38 +1030

    Abstract functions, remove load on open file (causing bugs)

--- src/extension.ts
+++ src/extension.ts
@@ -4,20 +4,20 @@ import {TextDocument, Uri} from 'vscode';
 
 let angularExtensions: string[] = ['ts', 'html', 'css', 'scss'];
 export function activate(context: vscode.ExtensionContext) {
-  vscode.workspace.onDidOpenTextDocument(
-      (file: any) => {openRelevantFiles(file)});
   let cmdOpen =
       vscode.commands.registerCommand('angularFileLoader.open', () => {
-        vscode.workspace.openTextDocument().then(file => {
-          openRelevantFiles(file);
-        });
+        let activeDocument: TextDocument =
+            vscode.window.activeTextEditor.document;
+        openRelevantFiles(getPath(activeDocument));
       });
   context.subscriptions.push(cmdOpen);
 }
 
-export function openRelevantFiles(file: TextDocument) {
+export function getPath(file: TextDocument) {
+  console.log('file:', file);
   // Get the file extension
   let ext = file.fileName.split('.').pop();
+  console.log('ext', ext);
   // If the extension is not in the specified files to open, return
   if (angularExtensions.indexOf(ext) === -1) {
     return;
@@ -26,11 +26,12 @@ export function openRelevantFiles(file: TextDocument) {
   // `/src/app/home/home.component.*` if `home.component.ts` is opened
   let relatedFiles =
       file.fileName.substr(0, file.fileName.length - ext.length) + '*';
-  // The file path relevant to the workspace
-  let relativeRelatedFiles = vscode.workspace.asRelativePath(relatedFiles);
+  return vscode.workspace.asRelativePath(relatedFiles);
+}
+export function openRelevantFiles(fileString: string) {
   // Iterate through relevant files and open them if they are within the set
   // extensions
-  vscode.workspace.findFiles(relativeRelatedFiles).then((files: Uri[]) => {
+  vscode.workspace.findFiles(fileString).then((files: Uri[]) => {
     files.forEach((f: Uri) => {
       if (angularExtensions.indexOf(f.path.split('.').pop()) !== -1) {
         openFile(f);
